e97d775464ae5e03c66bcba2c380ba643b734b66
Dennis515jd <dennis515jd@gmail.com>
2023-09-22 22:30:57 +0800

    build2

--- class/MapLoader.gd
+++ class/MapLoader.gd
@@ -1,144 +1,140 @@
 class_name MapLoader
 extends Resource
 var thread = Thread.new()
-
 var CellSize:int = Global.CellSize #格子尺寸 pix^2
 var ChunkSize:int = Global.ChunkSize #區域尺寸 cell^2
 var MapSize:int = Global.MapSize #地圖尺寸 chunk^2
-var LoadSize:int = Global.LoadSize: #加載區塊半徑
-	set(new):
-		var r = (new*2+1)
-		var output = []
-		for x in range(r):
-			for y in range(r):
-				output.append(To1DArry(Vector2(x-new, y-new)))
-		LoadArray = output.duplicate(true)
-		LoadSize = new
-		
-			
-var type:String #讀取類別
-var Map_Mutex = Mutex.new()
-var MapArray:Array = []
-var ChunkNodesArray:Array = []
+var LoadSize:int = Global.LoadSize #加載區塊半徑
+var Type:String #讀取類別
+var MapArray:Array #地圖存檔
+var ChunkNodesArray:Array #區塊實例
 var ResourcePack:Object #資源包
 var TargetNode:Object #目標節點
 
-var T_mutex = Mutex.new() #加載互斥鎖
-var TargetPosition:Vector2: #加載中心  需解鎖
-	set(new):
-		T_mutex.lock()
-		TargetPosition = new
-		T_mutex.unlock()
-	get:
-		T_mutex.lock()
-		return(TargetPosition)
-		T_mutex.unlock()
-var L_mutex = Mutex.new() #加載陣列互斥鎖
-var LoadArray = []: #加載陣列 需解鎖
-	set(new):
-		L_mutex.lock()
-		LoadArray = new
-		L_mutex.unlock()
-	get:
-		L_mutex.lock()
-		return(LoadArray)
-		L_mutex.unlock()
-
 
 #背景線程,實例化控制器
-var HalfCellSize:Vector2
-
-func _init(target:Object, ResourcePack:Object, mapdata, type:String):
-	LoadSize = Global.LoadSize
-	if typeof(mapdata) == TYPE_ARRAY:
-		MapArray = mapdata
+func _init(target:Object, resource_pack:Object, type:String, mapdata):
+	TargetNode = target
+	ResourcePack = resource_pack
+	Type = type
+	if (typeof(mapdata) == TYPE_ARRAY):
+		pass
 	else:
-		MapArray.resize((ChunkSize**2) * (MapSize**2))
-		MapArray.fill(0)
-		ChunkNodesArray.resize((ChunkSize**2) * (MapSize**2))
-		ChunkNodesArray.fill(0)
-	HalfCellSize = Vector2(CellSize/2, CellSize/2)
-	TargetNode = target #目標節點
-	
-
+		MapArray = EmptyMap(0)
+	ChunkNodesArray.resize(MapSize)
+	for i in range(MapSize):
+		var y =[]
+		y.resize(MapSize)
+		y.fill(0)
+		ChunkNodesArray[i] = y.duplicate()
+		
+		
+		
+		
 func _start_thread():
 	thread.start(_load_thread)
+	
 
+#---------------------跨線程同步資料-----------------
+var CenterPos:Vector2:
+	set(new):
+		CPmux.try_lock()
+		CenterPos = new
+		CPmux.unlock()
+var CPmux = Mutex.new()
 
-func _load_thread(): #背景加載線程
+#---------------------背景加載線程-------------------
+
+func _load_thread(): 
+	print("thread start")
 	var 已加載區塊:Array = []
-	var LastChunkPos:Vector2 = Vector2(-9999,-9999)
+	var LastChunkPos:Vector2i = Vector2i.ZERO
+	var CenterChunkpos = ToChunkPos(ToCellPos(CenterPos))
 	while (true):
 		await TargetNode.get_tree().process_frame
-		var pos = ToChunkPos(ToCellPos(TargetPosition))
-		if pos == Vector2i(LastChunkPos):
-			continue #同位置 加載結束
-		LastChunkPos = pos
-		var 緩存 = []
-		var 需加載 = [] #設定加載項
-		for i in LoadArray:
-			var id = Vector2(i+pos.x, i+MapSize*pos.y)
-			緩存.append(i)
-			print(i)
-			if not 已加載區塊.has(id):
-				需加載.append(id)
-		var 需卸載 = [] #設定卸載項
-		for i in 需加載:
-			if 已加載區塊.has(i):
-				需卸載.append(i)
+		CenterChunkpos = ToChunkPos(ToCellPos(CenterPos))
+		if 已加載區塊.size() != 0 and LastChunkPos == CenterChunkpos:
+			continue #加載區塊重複
+		LastChunkPos = ToChunkPos(ToCellPos(CenterPos))
+		var 需加載區塊 = LoadChunkArray(CenterChunkpos)
+		var 需刪除區塊 = []
+		var save = []
+		for 區塊 in 已加載區塊:
+			if 區塊 not in 需加載區塊:
+				需刪除區塊.append(區塊)
+		for 區塊 in 需加載區塊:
+			if 區塊 not in 已加載區塊:
+				save.append(區塊)
+		已加載區塊 = 需加載區塊
+		需加載區塊 = save
 		
-		已加載區塊 = 緩存.duplicate(true)
-		加載(需加載)
-		卸載(需卸載)
-
-
-func 加載(array:Array):
-	for i in array:
-		var output = Node.new()
-		ChunkNodesArray[i] = output
-		for k in ChunkToCell(To2DArry(i)):
-			var id = To1DArry(k)
-			if id >= MapArray.size() or id <= 0:
-				continue
-			var read = ResourcePack.data[type][MapArray[id]]
-			var inst = read.instantiate()
-			inst.position = ToWorldPos(To2DArry(id))
-			output.add_child(inst)
-		TargetNode.call_deferred("add_child",output)
+		for i in 加載(需加載區塊):
+			TargetNode.call_deferred("add_child", i)
+		for i in 刪除(需刪除區塊):
+			i.call_deferred("queue_free")
+		
+func 加載(load:Array):
+	var output = []
+	for i in load:
+		var node = Node.new()
+		var cells = ChunkToCell(i)
+		if i.x<0 or i.x>=MapSize or i.y<0 or i.y>=MapSize:
+			continue
+		for c in cells:
+			var inst = ResourcePack.data[Type][MapArray[c.x][c.y]].instantiate()
+			inst.position = ToWorldPos(c)
+			node.add_child(inst)
+		ChunkNodesArray[i.x][i.y] = node
+		output.append(node)
+	return output
 			
-func 卸載(array:Array):
-	for i in array:
-		TargetNode.call_deferred("queue_free",ChunkNodesArray[i])
-
-
-
-
-
-
-
-
-
-
-func ChunkToCell(pos:Vector2i): #區域座標 轉 方塊座標
+func 刪除(load):
+	var output = []
+	for i in load:
+		if i.x<0 or i.x>=MapSize or i.y<0 or i.y>=MapSize:
+			continue
+		if typeof(ChunkNodesArray[i.x][i.y]) != TYPE_OBJECT:
+			continue
+		output.append(ChunkNodesArray[i.x][i.y])
+		ChunkNodesArray[i.x][i.y] = 0
+	return (output)
+		
+#---------------------工具-----------------------------
+func LoadChunkArray(position:Vector2i) -> Array: #輸入區塊座標 輸出加載區塊陣列
+	var size
+	if LoadSize %2 ==0:
+		size = LoadSize
+	else: 
+		size = LoadSize+1
+	var output =[]
+	output.resize(size**2)
+	for x in range(size):
+		for y in range(size):
+			output[x*size+y] = Vector2i(x-size/2+position.x,y-size/2+position.y)
+	return (output)
+	
+func EmptyMap(with) -> Array:
+	var output:Array = []
+	output.resize(MapSize*ChunkSize)
+	for i in range(MapSize*ChunkSize):
+		var k = []
+		k.resize(MapSize*ChunkSize)
+		k.fill(with)
+		output[i] = k.duplicate(true)
+	return(output)
+	
+func ChunkToCell(pos:Vector2i) -> Array: #區域座標 轉 方塊座標
 	var output = []
 	for i in range(ChunkSize):
 		for k in range(ChunkSize):
 			output.append(Vector2i(i+pos.x*ChunkSize,k+pos.y*ChunkSize))
 	return(output)
-		
-func ToCellPos(position:Vector2): #世界座標 轉 方塊座標
+func ToCellPos(position:Vector2) -> Vector2i: #世界座標 轉 方塊座標
 	return(Vector2i(position/CellSize))
-func ToChunkPos(cell:Vector2): #方塊座標 轉 區域座標
+func ToChunkPos(cell:Vector2) -> Vector2i: #方塊座標 轉 區域座標
 	return(Vector2i(cell/ChunkSize))
-func ToWorldPos(cell:Vector2): #方塊座標 轉 世界座標
-	return(cell*CellSize + HalfCellSize)
-	
-func To1DArry(cell:Vector2): #區域座標 轉 一維陣列
-	return int(cell.x + MapSize*cell.y)
-func To2DArry(cell):
-	return(Vector2i(cell%MapSize, cell/MapSize))
-
-
+func ToWorldPos(cell:Vector2) -> Vector2: #方塊座標 轉 世界座標
+	return(cell*CellSize + Vector2(CellSize/2,CellSize/2))
 
 
 
--- main.gd
+++ main.gd
@@ -3,9 +3,11 @@ var ResourcePack
 var loader
 func _ready():
 	ResourcePack = resource.new()
-	loader = MapLoader.new($floor, ResourcePack, 0,"floor")
+	loader = MapLoader.new($floor, ResourcePack, "floor", 0)
 	loader._start_thread()
-var move = Vector2(0,0)
+var move = Vector2(100,100)
 func _process(delta):
-	loader.TargetPosition =move
-	move.x += 20*delta
+	loader.CenterPos =move
+	move.x += 1000*delta
+	move.y += 1000*delta
+	$Camera2D.position = move
